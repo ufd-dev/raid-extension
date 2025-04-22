d92bedeed896c47c39f0264060c341bdd22cd3a4
Just A. Guy <206441978+ufd-dev@users.noreply.github.com>
2025-04-21 22:17:41 -0700

    dynamic config for HMR vs. prod builds

---
+++ manifest.config.ts
@@ -0,0 +1,34 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+import packageJson from "./package.json";
+
+const { version } = packageJson;
+
+// Convert from Semver (example: 0.1.0-beta6)
+const [major, minor, patch, label = "0"] = version
+  // can only contain digits, dots, or dash
+  .replace(/[^\d.-]+/g, "")
+  // split into version parts
+  .split(/[.-]/);
+
+export default defineManifest(async (env) => {
+  const isDev = env.mode === "development";
+  return {
+    manifest_version: 3,
+    name: isDev ? "[DEV] UFD Raiding" : "UFD Raiding",
+    // up to four numbers separated by dots
+    version: `${major}.${minor}.${patch}.${label}`,
+    // semver is OK in "version_name"
+    version_name: version,
+    host_permissions: isDev ? ["<all_urls>"] : undefined,
+    icons: {
+      "128": "public/icon128.png",
+    },
+    permissions: ["sidePanel"],
+    background: {
+      service_worker: "src/background.ts",
+    },
+    side_panel: {
+      default_path: "src/sidepanel.html",
+    },
+  };
+});
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
 
-export default defineConfig({
+import manifest from "./manifest.config";
+
+export default defineConfig(({ command }) => ({
+  build: {
+    emptyOutDir: true,
+    outDir: command === "serve" ? "live-build" : "dist",
+  },
   legacy: {
     skipWebSocketTokenCheck: true,
   },
   plugins: [crx({ manifest })],
-});
+}));
